83491390e55744cbaa04f1f4150a076b62234ec9
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-05-09 18:01:57 +0600

    bvb

--- freelancing_cum_jobportal/src/app/models/user-session.ts
+++ freelancing_cum_jobportal/src/app/models/user-session.ts
@@ -1,1 +1,8 @@
-export interface UserSession {}
+export interface UserSession {
+  id?: number;
+  userId: number;
+  jwtToken: string;
+  deviceInfo: string;
+  ipAddress: string;
+  expiresAt: string;
+}
--- freelancing_cum_jobportal/src/app/models/user.ts
+++ freelancing_cum_jobportal/src/app/models/user.ts
@@ -1,1 +1,14 @@
-export interface User {}
+export interface User {
+    id?: number;
+    name: string;
+    email: string;
+    passwordHash?: string;
+    phone: string;
+    city: string;
+    area: string;
+    profileImage: string;
+    isVerified: boolean;
+    status: 'active' | 'suspended' | 'banned';
+    isDeleted: boolean;
+    createdAt: string;
+}
