9b7a20c01b328ffe4b804c383f11bc384fb34898
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-05-10 18:45:21 +0600

    fgfg

--- freelancing_cum_jobportal/src/app/guards/admin-guard.ts
+++ freelancing_cum_jobportal/src/app/guards/admin-guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth';
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
+export const adminGuard: CanActivateFn = () => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+
+  if (auth.isAdmin()) {
+    return true;
+  }
+
+  router.navigate(['/dashboard']);
+  return false;
 };
