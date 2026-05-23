73b45a7d1bf56c8d6ba116fec680735ca74b9c95
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-05-23 19:00:09 +0600

    fgyh

--- job-freelance-platform/src/app/services/auth.service.ts
+++ job-freelance-platform/src/app/services/auth.service.ts
@@ -1,6 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserModel } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {}
+export class AuthService {
+
+
+
+  constructor(private router: Router) { }
+
+  // Logout method
+  logout(): void {
+
+    // Remove user from localStorage
+    localStorage.removeItem('user');
+
+    // Redirect to login page
+    this.router.navigate(['/login']);
+  }
+
+  // Get current logged in user
+  getCurrentUser(): UserModel | null {
+
+    const userData = localStorage.getItem('user');
+
+    if (userData) {
+      return JSON.parse(userData);
+    }
+
+    return null;
+  }
+
+  // Check login status
+  isLoggedIn(): boolean {
+
+    return !!localStorage.getItem('user');
+  }
+
+
+}
--- job-freelance-platform/src/app/shared/topbar/topbar.component.ts
+++ job-freelance-platform/src/app/shared/topbar/topbar.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NotificationDropdownComponent } from '../notification-dropdown/notification-dropdown.component';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-topbar',
-  imports: [RouterLink, NotificationDropdownComponent],
+  imports: [RouterLink, NotificationDropdownComponent,CommonModule],
   templateUrl: './topbar.component.html'
 })
-export class TopbarComponent {
-  userName: string = 'John Doe';
-  userInitial: string = 'J';
+export class TopbarComponent implements OnInit {
+  userName: string = '';
+  userInitial: string = '';
+   constructor(private authService: AuthService
+    
+   ) { }
+  ngOnInit(): void {
+    // 1. Get the user data directly from the service
+    const user = this.authService.getCurrentUser();
+
+    // 2. Check if the user exists and has a name
+    if (user && user.name) {
+      this.userName = user.name;
+      this.userInitial = user.name.charAt(0).toUpperCase();
+    } else {
+      this.userName = 'Guest';
+      this.userInitial = 'G';
+    }
+  }
+
+  logout(): void {
+
+    this.authService.logout();
+
+  }
 }
